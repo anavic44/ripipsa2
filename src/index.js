1e6f50fbaaf04664021ea5a5bd79a4776e9a51fe
Ana Victoria Campos <a01567111@tec.mx>
2024-04-10 19:24:57 -0600

    Initial project commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+
+/*import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ARDisplay from './ARDisplay';
+// 2) Obtener referencia al componente root
+
+const elemento = document.getElementById('root');
+
+// 3) Instruir a React que tome control de este elemento
+const root = ReactDOM.createRoot(elemento);
+
+// 4) Crear el componente
+
+// 5) Mostrar el componente en la pantalla
+root.render(<ARDisplay />);
+*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // Make sure this imports App, not ARDisplay
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />); // This should render App, which includes ARDisplay
+
+
+
