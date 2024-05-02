d75cb4a1ba92fb669cdea8ed661a1b772ba39048
Guadalupe1211 <A01563124@tec.mx>
2024-05-02 10:40:50 -0600

    jala

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -8,25 +8,41 @@ import { NavBar } from './NavBar';  // Asegúrate de que la ruta es correcta
 import 'animate.css';
 import { ProjectCard } from "./ProjectCard";
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export const Projects2 = () => {
     const [projects, setProjects] = useState([]);
+    const [userData, setUserData] = useState(null);
+
+    const {userId} = useParams ();
 
     useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const { data } = await axios.get(`http://localhost:2023/api/user/${userId}`);
+                console.log("UserData:", data); // Verifica si los datos del usuario se están recibiendo correctamente
+                setUserData(data);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
         const fetchProjects = async () => {
             //const ids = [1, 2, 3, 4, 5, 6];  // IDs correspondientes a los objetos en la API
-            const projectFetches = (axios.get(`http://localhost:2023/api/load-all-objects`));
+            //const projectFetches = (axios.get(`http://localhost:2023/api/load-all-objects`));
+            
             try {
                 
-                const {data} = await(axios.get(`http://localhost:2023/api/load-all-objects`));
+                const {data} = await(axios.get(`http://localhost:2023/api/userAndProjects/${userId}`));
                 //const projectsData = responses.map(response => response.data);
-                console.log(data)
+                console.log(data.projects)
 
-                setProjects(data);
+                setProjects(data.projects);
             } catch (error) {
                 console.error("Error fetching project data: ", error);
             }
         };
+        fetchUserData();
         fetchProjects();
     }, []);
 
@@ -40,7 +56,7 @@ export const Projects2 = () => {
                         <TrackVisibility>
                         {({ isVisible }) => (
                         <div className={isVisible ? "animate_animated animate_fadeIn" : ""}>
-                            <h2>Proyectos</h2>
+                            <h2>Proyectos de {userData ? userData.username : ""}</h2>
                             <p>Los proyectos disponibles para tu usuario son:</p>
                             <Tab.Container id="projects-tabs" defaultActiveKey="first">
                                 <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
